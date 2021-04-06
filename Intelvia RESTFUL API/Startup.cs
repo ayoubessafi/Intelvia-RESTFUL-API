3fbe2e10156242ae4505a9c7c5e9435e6eda1e7a
ayoubessafi <57940108+ayoubessafi@users.noreply.github.com>
2021-04-06 09:27:39 +0100

    connection with SQL server and injenction services in startup

--- Intelvia RESTFUL API/Startup.cs
+++ Intelvia RESTFUL API/Startup.cs
@@ -1,8 +1,11 @@
 using Intelvia_RESTFUL_API.Helpers;
+using Intelvia_RESTFUL_API.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,6 +31,11 @@ namespace Intelvia_RESTFUL_API
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<Jwt>(Configuration.GetSection("JWT"));
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
