a009faf68a3836f3d57a1caf31bf5bc90d112e64
ayoubessafi <57940108+ayoubessafi@users.noreply.github.com>
2021-04-06 10:12:49 +0100

    create Interface and class for Implemntation this Interface and injention theme in startup

---
+++ Intelvia RESTFUL API/Services/AuthService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intelvia_RESTFUL_API.Services
+{
+    public class AuthService : IAuthService
+    {
+    }
+}
