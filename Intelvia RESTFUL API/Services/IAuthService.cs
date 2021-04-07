3e13d39e8ebb26903fca3b84436d89ed0790439e
ayoubessafi <57940108+ayoubessafi@users.noreply.github.com>
2021-04-07 16:35:45 +0100

    create tasks and methodes

--- Intelvia RESTFUL API/Services/IAuthService.cs
+++ Intelvia RESTFUL API/Services/IAuthService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Intelvia_RESTFUL_API.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,8 @@ namespace Intelvia_RESTFUL_API.Services
 {
     public interface IAuthService
     {
+        Task<AuthModel> RegisterAsync(RegisterModel model);
+        Task<AuthModel> GetTokenAsync(TokenRequestModel model);
+        Task<string> AddRoleAsync(AddRoleModel model);
     }
 }
